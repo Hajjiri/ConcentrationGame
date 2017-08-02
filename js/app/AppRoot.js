ee0b3961c0c7a9e6a32e6f46c084b8dfb58073cb
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-02 19:59:11 +0800

    Initial commit

---
+++ js/app/AppRoot.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class ApplicationRoot extends Component {
+    render() {
+        return (
+            <View>
+                <Text> hello </Text>
+            </View>
+        );
+    }
+}
