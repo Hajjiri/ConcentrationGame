23cb5ace17c58701a11f832c47c483e4a238646e
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-04 00:42:10 +0800

    Adds FB login button component

---
+++ js/components/FBLogin.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import PropTypes from 'prop-types';
+
+export default class FBLogin extends Component {  
+  render() {
+    return (
+      <LoginButton
+        publishPermissions={["publish_actions"]}
+        onLoginFinished={
+          (error, result) => {
+            if (error) {
+              this.props.OnLoginFailed(result.error);
+            } else if (result.isCancelled) {              
+              this.props.OnLoginCanceled();
+            } else {
+              AccessToken.getCurrentAccessToken().then(
+                (data) => {
+                  this.props.OnLoginSucceed(data.accessToken.toString());
+                }
+              )
+            }
+          }
+        }
+        onLogoutFinished={() => this.props.onLogout()} />
+      //TODO: handle logout error, and result cases if possible?
+    );
+  }
+}
+FBLogin.propTypes = {
+  OnLoginSucceed: PropTypes.func,
+  OnLoginFailed: PropTypes.func,
+  OnLoginCanceled: PropTypes.func,
+  onLogout: PropTypes.func
+};
