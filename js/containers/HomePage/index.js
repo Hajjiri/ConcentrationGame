5f123b72df90e8279c26aa00c28646564d395ec1
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-04 02:05:42 +0800

    Adds fb button at Home page, for allows users to logout

--- js/containers/HomePage/index.js
+++ js/containers/HomePage/index.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import autobind from 'autobind-decorator';
+import FBLogin from '@components/FBLogin'
+import { Toastr } from '@components/Toastr';
+import { resetStack } from '@app_init/NavigationActionsMethods';
+
 export default class HomePage extends Component {
     static navigationOptions = ({ navigation, screenProps }) => ({
         title: 'Best Game Ever Yet!'
     });
-    constructor(props) {
-        super(props);
-    }
 
-    // this.props.navigation.navigate('HomePage');    
+    @autobind
+    onLogout() {
+        Toastr.makeToast("Logged out successfully.");
+        resetStack(this.props.navigation, "LoginPage");
+    }
 
     render() {
         return (
-            <View>
-                <View style={styles.container}>
-                    <Text>Wow. It really works!</Text>
-                </View>                
+            <View style={styles.container}>
+                <Text style={styles.login_header}>Wow. It really works! Hello, {this.props.navigation.state.params.user.userId}</Text>
+                <FBLogin
+                    onLogout={this.onLogout}
+                />
             </View>
         );
     }
