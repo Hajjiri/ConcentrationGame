376756eaeac822dfd72fa383dd382dd6d9a2aea3
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-07 23:01:50 +0800

    changes images uri's, so it be consumable from react image component

--- js/configurations/game_settings.js
+++ js/configurations/game_settings.js
@@ -1,30 +1,50 @@
 const settings = {
-    easyDifficulty: {
-        sideSize: 2,
-        repetition: 2,
-        cells: {
-            images: [
-                'https://dummyimage.com/60x60/613c61/cccdd9.png&text=1',
-                'https://dummyimage.com/60x60/807780/cccdd9.png&text=2'
-            ]
+  easyDifficulty: {
+    sideSize: 2,
+    repetition: 2,
+    cells: {
+      images: [
+        {
+          uri: "https://dummyimage.com/60x60/613c61/cccdd9.png&text=1"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/807780/cccdd9.png&text=2"
         }
-    },
-    hardDifficulty: {
-        sideSize: 8,
-        repetition: 2,
-        cells: {
-            images: [
-                'https://dummyimage.com/60x60/613c61/cccdd9.png&text=1',
-                'https://dummyimage.com/60x60/807780/cccdd9.png&text=2',
-                'https://dummyimage.com/60x60/262326/cccdd9.png&text=3',
-                'https://dummyimage.com/60x60/150042/cccdd9.png&text=4',
-                'https://dummyimage.com/60x60/820e1c/cccdd9.png&text=5',
-                'https://dummyimage.com/60x60/0994f0/cccdd9.png&text=6',
-                'https://dummyimage.com/60x60/0c9e7c/cccdd9.png&text=7',
-                'https://dummyimage.com/60x60/d99503/cccdd9.png&text=8'
-            ]
-        }
+      ]
     }
-}
+  },
+  hardDifficulty: {
+    sideSize: 8,
+    repetition: 2,
+    cells: {
+      images: [
+        {
+          uri: "https://dummyimage.com/60x60/613c61/cccdd9.png&text=1"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/807780/cccdd9.png&text=2"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/262326/cccdd9.png&text=3"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/150042/cccdd9.png&text=4"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/820e1c/cccdd9.png&text=5"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/0994f0/cccdd9.png&text=6"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/0c9e7c/cccdd9.png&text=7"
+        },
+        {
+          uri: "https://dummyimage.com/60x60/d99503/cccdd9.png&text=8"
+        }
+      ]
+    }
+  }
+};
 
 export default settings;
