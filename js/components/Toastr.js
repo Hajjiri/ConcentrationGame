ee02d7eadb172b55a522070269fed9a44a4205c0
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-11 17:16:15 +0800

    enables the support of ios toast messages

--- js/components/Toastr.js
+++ js/components/Toastr.js
@@ -1,14 +1,28 @@
-import _ from 'lodash';
-import { ToastAndroid } from 'react-native';
+import _ from "lodash";
+import { ToastAndroid, Platform } from "react-native";
+import Toast from "react-native-root-toast";
+
+var defaultOption = {
+    duration: Toast.durations.SHORT,
+    position: -100,
+    shadow: true,
+    animation: true,
+    hideOnPress: true,
+    delay: 0
+};
 
 export var Toastr = {
-    makeToast: function (message) {     
-        if(_.isEmpty(message)){
-            message = "Error. Please contact that bloody developer..";
-        }  
-        if (typeof message === "object") {
-            message = JSON.stringify(message);
-        }
-        ToastAndroid.show(message, ToastAndroid.SHORT);
+  makeToast: function(message) {
+    if (_.isEmpty(message)) {
+      message = "Error. Please contact that bloody developer..";
     }
+    if (typeof message === "object") {
+      message = JSON.stringify(message);
+    }
+    if (Platform.OS === "ios") {
+      Toast.show(message, defaultOption);
+    } else {
+      ToastAndroid.show(message, ToastAndroid.SHORT);
+    }
+  }
 };
