1f437f5345daa9714140fa5db634c26fe630bd13
MHD HASAN HAJJIRI <hajjiri.hasan@gmail.com>
2017-08-12 15:29:24 +0800

    Redirects user to a new score screen after game finishes

---
+++ js/containers/ScorePage/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: 'white',
+        flex: 1,
+        borderWidth: 3,
+        borderColor: '#9C8AA5'
+    },
+    content: {
+        alignItems: 'center'
+    },
+    header: {
+        textAlign: 'center',
+        margin: 10,
+        color: '#9C8AA5'
+    }
+});
