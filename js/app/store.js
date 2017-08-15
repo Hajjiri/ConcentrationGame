0af26f24a1894e662693b000497c9553ae9cd2d8
Hajjiri <hajjiri.hasan@gmail.com>
2017-08-15 21:55:18 +0800

    Delaying App rendering untill store Rehydration is complete with a loading screen to improve smoothieness when the app starts

--- js/app/store.js
+++ js/app/store.js
@@ -1,13 +1,11 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import { createLogger } from "redux-logger";
 import mainReducer from "@reducers";
-import { persistStore, autoRehydrate } from "redux-persist";
+import { autoRehydrate } from "redux-persist";
 import thunk from "redux-thunk";
 import promise from "redux-promise-middleware";
 
 const middlewares = applyMiddleware(promise(), thunk, createLogger({}));
 const store = createStore(mainReducer, compose(middlewares, autoRehydrate()));
 
-import { AsyncStorage } from "react-native";
-persistStore(store, { storage: AsyncStorage });
 export default store;
