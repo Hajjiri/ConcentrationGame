af42919716a34576acce2d28c5260facbc266d72
MHD HASAN HAJJIRI <hajjiri.hasan@gmail.com>
2017-08-15 21:55:18 +0800

    Delaying App rendering untill store Rehydration is complete with a loading screen to improve smoothieness when the app starts

--- js/app/AppRoot.js
+++ js/app/AppRoot.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import { View } from "react-native";
 import { Provider } from "react-redux";
+import { AsyncStorage } from "react-native";
+import { persistStore } from "redux-persist";
+import Spinner from "react-native-loading-spinner-overlay";
 import AppWithNavigationState from "./App";
 import store from "./store";
 
 export default class ApplicationRoot extends Component {
+  constructor() {
+    super();
+    this.state = { rehydrated: false };
+  }
+  componentWillMount() {
+    persistStore(store, { storage: AsyncStorage }, () => {
+      this.setState({ rehydrated: true });
+    });
+  }
   render() {
+    if (!this.state.rehydrated) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Spinner
+            visible={true}
+            textContent={"Loading game data. Please wait..."}
+            textStyle={{ color: "#421C52" }}
+            color={"#421C52"}
+            overlayColor={"rgba(0, 0, 0, 0.25)"}
+            // cancelable={true}
+          />
+        </View>
+      );
+    }
     return (
       <Provider store={store}>
         <AppWithNavigationState />
